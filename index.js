b754b0d847ab1020b1baafeb004de813a0296bfd
CTECStudent <ctecstudent@h8-24-751289.abcadultschool.k12.ca.us>
2022-05-26 11:03:11 -0700

    Adding places controller

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 require('dotenv').config();
+const express = require('express');
+const app = express()
 
-// 8. Require express at the top of the file
-    const express = require('express');
-// 9. Initialize the app variable
-    const app = express()
-// 10. Create the home page route
-//    a. Call app.get()
-//    b. Set ‘/‘ as the path (first arg)
-//    c. Write callback function with req, res
-//    d. Call res.send(‘hello world’) 
-    app.get('/', (req, res) => {
+app.use('/places', require('./controlles/places'))
+
+app.get('/', (req, res) => {
         res.send('Hello World!')
-    })
+})
 
-    app.get('*', (req, res) => {
+app.get('*', (req, res) => {
         res.status(404).send('<h1>404 Not Found</h1>')
-    })
-// 11. Call app.listen(3000) to keep server open
-    app.listen(process.env.PORT)
+})
+
+app.listen(process.env.PORT)
