8d817237e8e23f4f509294d72a2f9dca67081feb
AngelTheCreator <angel_guevara94@hotmail.com>
2022-06-16 16:48:43 -0700

    Adding styles to Vegan REST-rant

--- controllers/places.js
+++ controllers/places.js
@@ -5,18 +5,21 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
     let places = [{
+      id: 1,
       name: 'Vihn loi Tofu',
       city: 'Cerritos',
       state: 'CA',
       cuisines: 'Vietnamese, Vegan',
       pic: 'https://www.presstelegram.com/wp-content/uploads/2019/07/LPT-L-DINE-VINHLOI-1-1.jpg?w=1024'
     }, {
+      id: 2,
       name: 'Nice Guys',
       city: 'Stanton',
       state: 'CA',
       cuisines: 'American, Vegan',
       pic: 'https://static.wixstatic.com/media/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg/v1/fill/w_2500,h_1875,al_c/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg'  
     }, {
+      id: 3,
       name: 'Tocaya',
       city: 'Long Beach',
       state: 'CA',
