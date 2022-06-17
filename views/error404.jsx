7dd367a29aad9b641b34d5721ff957ddcd67d66e
AngelTheCreator <angel_guevara94@hotmail.com>
2022-06-16 17:50:47 -0700

    Adding Bootstrap and styling

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,8 +5,19 @@ function error404 () {
     return (
         <Def>
             <main>
-                <h1>404 Error: Sorry! Your page is in another castle</h1>
-                <img src="/images/supermario.gif" alt="super mario image, princess in another castle"/>
+                <h1>404 Error: Sorry! Your plant is in another website</h1>
+                <img src="images/404.jpeg" alt="Woman hiding begind plants" />
+                <div className="link">
+                 <a href="Photo by Daria Shevtsova: https://www.pexels.com/photo/person-holding-green-vegetables-3629537/">Photo</a> by <a href="https://www.pexels.com/@daria/">Daria Shevtsova</a> on <a href="https://www.pexels.com/">Pexels</a>
+                </div>
+                <div className="container"> 
+                        <div className="center"> 
+                <a href="/">
+                    <button className="btn-primary">Go Back</button>
+                </a>
+                        </div>  
+                    </div>
+                <div></div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -5,10 +5,21 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>Vegone</h1>
+                <h2>Your hub for all things Plant-Based</h2>
+                    <div className="container"> 
+                        <div className="center"> 
                 <a href="/places">
-                    <button className="btn-primary">Places Page</button>
+                    <button className="btn-primary">Vegin Your Journey</button>
                 </a>
+                        </div>  
+                    </div>
+                <div>
+                    <img src="/images/green.jpeg" alt="Green neon sign surrounded by plants"/>
+                </div>
+                <div className="link">
+                 <a href="https://www.pexels.com/photo/illuminated-green-sign-3626733/">Photo</a> by <a href="https://www.pexels.com/@daria/">Daria Shevtsova</a> on <a href="https://www.pexels.com/">Pexels</a>
+                </div>
             </main>
         </Def>
     )
