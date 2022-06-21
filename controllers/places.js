e169bfd1ea01ace134b825ff10b42a99ac388972
AngelTheCreator <angel_guevara94@hotmail.com>
2022-06-21 16:39:58 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const { application } = require('express')
+const places = require('../models/places')
 
 const router = require('express').Router()
 
@@ -8,32 +9,21 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST/places')
+  if (!req.body.pic){
+    //Default image if one is not provided
+    req.body.pic = 'https://images.pexels.com/photos/3669638/pexels-photo-3669638.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state){
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 router.get('/', (req, res) => {
-    let places = [{
-      id: 1,
-      name: 'Vihn loi Tofu',
-      city: 'Cerritos',
-      state: 'CA',
-      cuisines: 'Vietnamese, Vegan',
-      pic: 'https://www.presstelegram.com/wp-content/uploads/2019/07/LPT-L-DINE-VINHLOI-1-1.jpg?w=1024'
-    }, {
-      id: 2,
-      name: 'Nice Guys',
-      city: 'Stanton',
-      state: 'CA',
-      cuisines: 'American, Vegan',
-      pic: 'https://static.wixstatic.com/media/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg/v1/fill/w_2500,h_1875,al_c/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg'  
-    }, {
-      id: 3,
-      name: 'Tocaya',
-      city: 'Long Beach',
-      state: 'CA',
-      cuisines: 'Mexican, Vegan',
-      pic: 'https://images.getbento.com/accounts/d7955fbecf5892a7c71ad78974658f55/media/images/41389Patio.jpg?w=1800&fit=max&auto=compress,format&h=1800'
-    }]
       res.render('places/index', {places})
   });
 
---
+++ models/places.js
@@ -0,0 +1,22 @@
+module.exports = [{
+    id: 1,
+    name: 'Vihn loi Tofu',
+    city: 'Cerritos',
+    state: 'CA',
+    cuisines: 'Vietnamese, Vegan',
+    pic: 'https://www.presstelegram.com/wp-content/uploads/2019/07/LPT-L-DINE-VINHLOI-1-1.jpg?w=1024'
+  }, {
+    id: 2,
+    name: 'Nice Guys',
+    city: 'Stanton',
+    state: 'CA',
+    cuisines: 'American, Vegan',
+    pic: 'https://static.wixstatic.com/media/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg/v1/fill/w_2500,h_1875,al_c/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg'  
+  }, {
+    id: 3,
+    name: 'Tocaya',
+    city: 'Long Beach',
+    state: 'CA',
+    cuisines: 'Mexican, Vegan',
+    pic: 'https://images.getbento.com/accounts/d7955fbecf5892a7c71ad78974658f55/media/images/41389Patio.jpg?w=1800&fit=max&auto=compress,format&h=1800'
+  }]
