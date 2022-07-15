835228dda25b85d6b3f5911e089e7561eb6a98f0
AngelTheCreator <angel_guevara94@hotmail.com>
2022-07-14 21:15:42 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -40,4 +40,17 @@ router.get('/', (req, res) => {
     }
   })
 
+  router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -26,7 +26,7 @@ function show (data) {
                     <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                             Edit
                         </a>
-                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                        <form method='DELETE' action={`/places/${data.id}?_method=DELETE`}>
                             <button type='submit' className='btn btn-danger'>
                                 Delete
                             </button>
