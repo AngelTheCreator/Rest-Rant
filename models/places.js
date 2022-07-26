7dcfd9fc01c1b304bcb6a231597c6389268a1314
AngelTheCreator <angel_guevara94@hotmail.com>
2022-07-25 22:12:15 -0700

    set up mongo, schema and updated index and show

--- models/places.js
+++ models/places.js
@@ -1,25 +1,12 @@
-module.exports = [{
-    id: 0,
-    name: 'Vihn loi Tofu',
-    city: 'Cerritos',
-    state: 'CA',
-    cuisines: 'Vietnamese Cuisine',
-    pic: 'https://www.presstelegram.com/wp-content/uploads/2019/07/LPT-L-DINE-VINHLOI-1-1.jpg?w=1024',
-    site: 'https://www.vinhloitofu.com/'
-  }, {
-    id: 1,
-    name: 'Nice Guys',
-    city: 'Stanton',
-    state: 'CA',
-    cuisines: 'American Cuisine',
-    pic: 'https://static.wixstatic.com/media/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg/v1/fill/w_2500,h_1875,al_c/dadd24_175bbfb9106b4386b9d22609053d5386~mv2.jpg',
-    site: 'https://www.niceguysvegan.com/'  
-  }, {
-    id: 2,
-    name: 'Tocaya',
-    city: 'Long Beach',
-    state: 'CA',
-    cuisines: 'Mexican Cuisine',
-    pic: 'https://images.getbento.com/accounts/d7955fbecf5892a7c71ad78974658f55/media/images/41389Patio.jpg?w=1800&fit=max&auto=compress,format&h=1800',
-    site: 'https://tocaya.com/menu/'
-  }]
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+      name: { type: String, required: true },
+      pic: String,
+      cuisines: { type: String, required: true},
+      city: { type : String, default: 'Anytown'},
+      state: { type: String, default: 'USA'},
+      founded: Number
+});
+
+module.exports = mongoose.model('Place', placeSchema)
