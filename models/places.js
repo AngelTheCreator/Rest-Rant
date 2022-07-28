071149bffeddf1dd6dd633a4c744281cb4a6b038
AngelTheCreator <angel_guevara94@hotmail.com>
2022-07-28 16:14:53 -0700

    added validations to the form

--- models/places.js
+++ models/places.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 const placeSchema = new mongoose.Schema({
       name: { type: String, required: true },
-      pic: String,
+      pic: { type: String, default: 'https://images.pexels.com/photos/3669638/pexels-photo-3669638.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
       cuisines: { type: String, required: true},
       city: { type : String, default: 'Anytown'},
       state: { type: String, default: 'USA'},
-      founded: Number
+      founded: {
+            type: Number,
+            min: [803, 'Is it really that old?'],
+            max: [new Date().getFullYear(), 'You made a time machine out of a DeLorean? Because this year is in the future']
+      }
 });
 
 placeSchema.methods.showEstablished = function() {
