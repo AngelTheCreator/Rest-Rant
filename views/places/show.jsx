1d36da49d1bf3a104f8abacb7ed1a7c291402ec2
AngelTheCreator <angel_guevara94@hotmail.com>
2022-07-28 13:42:29 -0700

    create and use a helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,8 +7,20 @@ function show (data) {
             <main>
                 <h1>{data.place.name}</h1>
                 <img src={data.place.pic} alt={data.place.name}/>
-                <h4>{data.place.cuisines}</h4>
-                <h5>{data.place.city}, {data.place.state}</h5>
+                <h4>Serving {data.place.cuisines}</h4>
+                <div className='container'>
+                    <div className='center'>
+                    <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                            Edit
+                        </a>
+                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                            <button type='submit' className='btn btn-danger'>
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                    </div>
+                <h4>{data.place.showEstablished()}</h4>
                 <h2>Rating</h2>
                 <p>Not Rated</p>
                 <h2>Comments</h2>
@@ -21,18 +33,6 @@ function show (data) {
                     </div>
                 </div>
                 
-                        <div className='container'>
-                    <div className='center'>
-                    <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
-                            Edit
-                        </a>
-                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-                            <button type='submit' className='btn btn-danger'>
-                                Delete
-                            </button>
-                        </form>
-                    </div>
-                    </div>
             </main>
         </Def>
     )
