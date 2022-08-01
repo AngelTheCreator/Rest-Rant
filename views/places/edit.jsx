3b133ef0837fb219165d82e27cf735dbac33480e
AngelTheCreator <angel_guevara94@hotmail.com>
2022-08-01 12:36:05 -0700

    both the delete and edit buttons/routes are working perfectly

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                     <div className='row' style={{'textAlign': 'center'}}>
                     <div className='col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor="name">Place Name</label>
@@ -51,10 +51,17 @@ function edit_form (data) {
                         value={data.place.cuisines}
                         required />
                     </div>
+                    <div className='form-group col-sm-4'>
+                        <label htmlFor='founded'>Founded</label>
+                        <input className='form-control'
+                        id='founded'
+                        name='founded'
+                        value={data.place.founded}/>
+                    </div>
                     </div>
                     <div className="container"> 
                         <div className="center"> 
-                        <input className='btn btn-primary' type="submit" value="Edit" />
+                        <input className='btn btn-primary' type="submit" value="Update Place" />
                         </div>
                         </div>
                     </form>
